08fbb4dddd347fe3fdcc7c4ff03f08583fa4b480
HookSander <hooksander@HookSander-Kali.WORK>
2022-07-19 11:53:33 +0200

    Active and scan bluetooth device available

---
+++ bluetooth.sh
@@ -0,0 +1,39 @@
+#! /bin/share
+
+power_on() {
+    echo "power on" | bluetoothctl | grep "done" | cut -d " " -f 2
+}
+
+scan_on() {
+    echo "scan on" | bluetoothctl | grep "scan" | cut -d " " -f 2
+}
+
+echo -e "active bluetooth ? [y/n]"
+read ask
+
+if [ $ask == y ]; then
+    echo "activate bluetooth..."
+    sudo service bluetooth start
+else
+    echo "stop bluetooth..."
+    sudo service bluetooth stop
+    echo "done"
+    exit
+fi
+
+echo -e "done\ndo you want see status ? [y/n]\n=>"
+read ask1
+
+if [ $ask1 == y ]; then
+    service bluetooth status
+fi
+
+echo -e "start scan ? [y/n]\n=>"
+read ask2
+
+if [ $ask2 == y ]; then
+    echo "init..."
+    bluetoothctl
+    (power_on)
+    (scan_on)
+fi
